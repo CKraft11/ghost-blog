029ae4f9d08c37c1b03efe17cecc13708a4738c7
CKraft11 <cadenkraft@gmail.com>
2022-02-22 10:51:56 -0600

    Tue 22 Feb 2022 10:51:48 AM CST

--- ghost-update.sh
+++ ghost-update.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-date=$(date) 
-cd /home/radon_user/docs/static 
-git pull origin master 
-cd /home/radon_user/docs/
-sleep 5 
-gssg --productionDomain https://debug.cadenkraft.com
-cd /home/radon_user/docs/static 
-sleep 5 
+date=$(date)
+cd /home/radon_user/ghost git pull origin master 
+rm -r /home/radon_user/ghost/docs 
+sleep 2 
+gssg --productionDomain https://debug.cadenkraft.com --dest docs 
+sleep 2 
 git add . 
 sleep 2 
 git commit -m "$date" 
