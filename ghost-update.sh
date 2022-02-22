f4ee6bd40e7953eeffe08fa0318e24d38e9a18aa
CKraft11 <cadenkraft@gmail.com>
2022-02-22 16:22:01 -0600

    Tue 22 Feb 2022 04:21:58 PM CST

--- ghost-update.sh
+++ ghost-update.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 date=$(date)
-cd /home/radon_user/ghost 
+cd /home/radon_user/ghost/docs
 echo "debug.cadenkraft.com" > CNAME
+cd /home/radon_user/ghost
 git pull origin master 
 rm -r /home/radon_user/ghost/docs 
 gssg --sourceDomain http://localhost:2367 --productionDomain http://debug.cadenkraft.com --dest docs
 git add . 
-git update-index --assume-unchanged CNAME
-git checkout CNAME
 git commit -m "$date" 
 git config --global credential.helper store
 git push -u origin master
