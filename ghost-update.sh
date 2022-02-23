4c210f9ddd396e022327bcdd783afeb8b508ae4b
CKraft11 <cadenkraft@gmail.com>
2022-02-23 00:15:37 -0600

    Wed 23 Feb 2022 12:15:35 AM CST

--- ghost-update.sh
+++ ghost-update.sh
@@ -8,6 +8,7 @@ echo "debug.cadenkraft.com" > CNAME
 cd -
 gssg --dest docs
 cd docs/
+docker cp ghost:/var/lib/ghost/content/images/. static/content/images
 grep -lR "10.0.0.42:2368" . | xargs sed -i 's/10.0.0.42:2368/debug.cadenkraft.com/g'
 cd -
 git add .
