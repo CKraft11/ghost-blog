9e8f1d57007d496e3f94635c109ab1d46675bfcb
CKraft11 <cadenkraft@gmail.com>
2022-02-24 12:23:46 -0600

    Thu 24 Feb 2022 12:23:45 PM CST

--- ghost-update.sh
+++ ghost-update.sh
@@ -7,7 +7,7 @@ cd docs
 echo "blog.cadenkraft.com" > CNAME
 cd -
 ECTO1_SOURCE=http://10.0.0.42:2368 ECTO1_TARGET=https://blog.cadenkraft.com python3 ecto1.py
-grep -lR "srcset" docs/ | xargs sed -i 's/srcset/thisisbuggedatm/g'
+# grep -lR "srcset" docs/ | xargs sed -i 's/srcset/thisisbuggedatm/g'
 git add .
 git commit -m "$date"
 git config --global credential.helper store
