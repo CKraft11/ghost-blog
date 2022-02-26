b5827024b202cfb2d0d65309d55de469e2c0db72
CKraft11 <cadenkraft@gmail.com>
2022-02-25 23:12:55 -0600

    Fri 25 Feb 2022 11:12:52 PM CST

--- ghost-update.sh
+++ ghost-update.sh
@@ -9,6 +9,7 @@ cd -
 ECTO1_SOURCE=http://10.0.0.42:2368 ECTO1_TARGET=https://blog.cadenkraft.com python3 ecto1.py
 cd docs
 docker cp ghost:/var/lib/ghost/content/images/. content/images
+docker cp ghost:/var/lib/ghost/content/renders/. content/renders
 cd -
 # grep -lR "srcset" docs/ | xargs sed -i 's/srcset/thisisbuggedatm/g'
 git add .
