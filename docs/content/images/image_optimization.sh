f3b26ab5110a2b7834b71cadd88f391ecfc2dd64
CKraft11 <cadenkraft@gmail.com>
2024-02-29 10:06:00 -0800

    Compiled Changes - Thu 29 Feb 2024 10:02:19 AM PST | Images converted to webp

--- docs/content/images/image_optimization.sh
+++ docs/content/images/image_optimization.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Improved as per feedback from @pascal - https://gist.github.com/julianxhokaxhiu/c0a8e813eabf9d6d9873#gistcomment-3086462
-find . -type f -iname "*.webp=" -exec opt.webp= -nb -nc {} \;
-#find . -type f -iname "*.webp=" -exec ad.webp= -z4 {} \;
-find . -type f -iname "*.webp=" -exec.webp=crush -rem gAMA -rem alla -rem cHRM -rem iCCP -rem sRGB -rem time -ow {} \;
-find . -type f \( -iname "*.webp=" -o -iname "*.webp=" \) -exec.webp=optim -f --strip-all {} \;
+find . -type f -iname "*.webp" -exec opt.webp -nb -nc {} \;
+#find . -type f -iname "*.webp" -exec ad.webp -z4 {} \;
+find . -type f -iname "*.webp" -exec.webpcrush -rem gAMA -rem alla -rem cHRM -rem iCCP -rem sRGB -rem time -ow {} \;
+find . -type f \( -iname "*.webp" -o -iname "*.webp" \) -exec.webpoptim -f --strip-all {} \;
