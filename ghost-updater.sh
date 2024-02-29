1003289f4b45790e985a0d880ac0eddae334051d
CKraft11 <cadenkraft@gmail.com>
2024-02-28 23:42:43 -0800

    Compiled Changes - Wed 28 Feb 2024 11:42:11 PM PST

--- ghost-updater.sh
+++ ghost-updater.sh
@@ -9,8 +9,8 @@ echo "cadenkraft.com" > CNAME
 cd -
 ECTO1_SOURCE=http://10.0.0.222:2368 ECTO1_TARGET=https://cadenkraft.com python3 ecto1.py
 cd docs
-cp -r /helium/ghost/content/images/. content/images
-cp -r /helium/ghost/content/renders/. content/renders
+cp -r /helium/ghost/ghost-backup/content/images/. content/images
+cp -r /helium/ghost/ghost-backup/content/renders/. content/renders
 cd -
 grep -lR "srcset=" docs/ | xargs sed -i 's/srcset=/thisisbuggedatm=/g'
 bash ./image_optimizer.sh
