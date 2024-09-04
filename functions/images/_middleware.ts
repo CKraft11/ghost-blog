0d11705074902d34dd85dfb8c299dfa0645dc55f
CKraft11 <cadenkraft@gmail.com>
2024-09-03 20:13:30 -0500

    adding image chooser for cf pages 3

--- functions/images/_middleware.ts
+++ functions/images/_middleware.ts
@@ -1,5 +1,5 @@
-import assetNegotiationPlugin from "../../..";
+import assetNegotiationPlugin from "pages-plugin-asset-negotiation";
 
 export const onRequest: PagesFunction = assetNegotiationPlugin({
-	formats: ['webp'],
+	formats: ['webp'], // The formats you want to support, in order of preference. This is the default configuration and will serve a jxl image if the browser supports it (and it was found) first, followed by avif and then webp.
 });
